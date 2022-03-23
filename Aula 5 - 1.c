80bd3f33a14b2b5cc31843c661146580e1c3d761
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2022-03-23 19:55:33 -0300

    Create Aula 5 - 1.c

---
+++ Aula 5 - 1.c
@@ -0,0 +1,7 @@
+/*
+@file: atividade1_aula5
+@author: Deivid da Silva Galvao
+@date:12 out 2021
+@brief: 1) Faça um programa que receba a altura de uma pessoa e
+calcule e exiba seu peso ideal utilizando a seguinte:
+peso ideal = 72.7 x altura 
