b91899e8c2590d0b4d95b0345529fac60ae214c8
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2022-03-30 20:39:15 -0300

    Create atividade1.c

---
+++ z.Aula 10 - For/atividade1.c
@@ -0,0 +1,23 @@
+/*
+@file: atividade1_aula10
+@author: Deivid da Silva Galvao
+@date: 10 nov 2021
+@brief: 1) Faça um algoritmo que exiba todos números de 1 a 100.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int contador;
+  for (contador = 1; contador <= 100; contador++) {
+  printf("%i\n",contador );
+}//fecha for
+
+
+
+
+
+  return 0;
+}//fecha main
